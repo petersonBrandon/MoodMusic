c44063a67f68c68670d5857a3276ff555f4c0183
Penelope <vilmag@byui.edu>
2022-03-09 09:29:40 -0800

    Added User Class

--- app/src/main/java/com/example/moodmusic/MainActivity.java
+++ app/src/main/java/com/example/moodmusic/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.moodmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText _txtUser, _txtPassword;
+    Button _bttnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        _txtPassword=(EditText) findViewById(R.id.txtPassword);
+        _txtUser=(EditText) findViewById(R.id.txtPassword);
+        _bttnLogin=(Button) findViewById(R.id.btnLogin);
+        _bttnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Take the User Credentials
+                if(_txtUser.getText().equals("vilma") && _txtPassword.getText().equals("Penelope1!")){
+                    Intent intent = new Intent (MainActivity.this, user.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
