236384b81e84f1240a0f4b532745ff9b281dd271
Penelope <vilmag@byui.edu>
2022-03-09 17:40:52 -0800

    Added class screens

---
+++ app/src/main/java/com/example/moodmusic/SelectSong.java
@@ -0,0 +1,14 @@
+package com.example.moodmusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectSong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_song);
+    }
+}
