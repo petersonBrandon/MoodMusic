9c168bc1bd2fa2ea11c3f7fc89f8ab67f7ce1be2
Penelope <vilmag@byui.edu>
2022-03-31 10:41:14 -0700

    Challenge Part-1

--- app/src/main/java/com/example/moodmusic/LikeList.java
+++ app/src/main/java/com/example/moodmusic/LikeList.java
@@ -2,13 +2,28 @@ package com.example.moodmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LikeList extends AppCompatActivity {
+    Button _btnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_like_list);
+
+        //Get Home after seeing the LikeList
+        _btnHome = (Button) findViewById(R.id.bntHome);
+        _btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LikeList.this, user.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
