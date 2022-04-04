bcae94a8540e4bc977af2ffbe2e0a788275c6eb3
Penelope <vilmag@byui.edu>
2022-04-04 06:38:58 -0700

    +Connection code

--- app/src/main/java/com/example/moodmusic/MainActivity.java
+++ app/src/main/java/com/example/moodmusic/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.moodmusic;
-
+//------SPOTIFY LIBRARIES -----
+//import com.spotify.android.appremote.api.ConnectionParams;
+//import com.spotify.android.appremote.api.Connector;
+//import com.spotify.android.appremote.api.SpotifyAppRemote;
+//import com.spotify.protocol.types.Track;
+//////////////////////////////////
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,10 +26,76 @@ import java.text.DateFormatSymbols;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //START CODE FOR CONNECTION
+//    // This is the connection to the API
+//    private static final String CLIENT_ID = "5e48b58845994c88aba86482d713c202"; // need to change to your client ID to work
+//    private static final String REDIRECT_URI = "com.example.mood2://callback";
+//    private SpotifyAppRemote mSpotifyAppRemote;
+//
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        ConnectionParams connectionParams =
+//                new ConnectionParams.Builder(CLIENT_ID)
+//                        .setRedirectUri(REDIRECT_URI)
+//                        .showAuthView(true)
+//                        .build();
+//
+//        SpotifyAppRemote.connect(this, connectionParams,
+//                new Connector.ConnectionListener() {
+//
+//                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+//                        mSpotifyAppRemote = spotifyAppRemote;
+//                        Log.d("MainActivity", "Connected! Yay!");
+//
+//                        // Now you can start interacting with App Remote
+//                        connected();
+//
+//                    }
+//
+//                    public void onFailure(Throwable throwable) {
+//                        Log.e("MyActivity", throwable.getMessage(), throwable);
+//
+//                        // Something went wrong when attempting to connect! Handle errors here
+//                    }
+//                });
+//    }
+//// Connect to a User account
+//
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+//    }
+//
+//    private void connected() {
+//        // Play a playlist
+//        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+//
+//        // Subscribe to PlayerState
+//        mSpotifyAppRemote.getPlayerApi()
+//                .subscribeToPlayerState()
+//                .setEventCallback(playerState -> {
+//                    final Track track = playerState.track;
+//                    if (track != null) {
+//                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+//                    }
+//                });
+
+
+
+//////////SCREENS/////////////////////////////
+
     EditText _txtUser, _txtPassword;
     Button _bttnLogin;
-//    ListView listView;
-//    String[] meses;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +118,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        listView= findViewById(R.id.listSongs);
-//        meses = new DateFormatSymbols().getMonths();
-//        ArrayAdapter<String> mesesAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,meses);
-     //   listView.setAdapter(mesesAdapter);
-
+        ///CODE FOR LIST TO BE IMPLEMENTED
 //        listView=(ListView) findViewById(R.id.listSongs);
 //        ArrayList<String> arrayList=new ArrayList<>();
 //        arrayList.add("Lo Mejor de Mi by Paul Mackanie");
